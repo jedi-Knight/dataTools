86c53e0552c6c7da9eb198a8d7fd51297460c318
Pratik Gautam <gautamxpratik@gmail.com>
2015-12-29 15:38:29 +0545

    ona/formhub key shortener

---
+++ formhub-output-cleaner/slashslasher.py
@@ -0,0 +1,44 @@
+#python3 remove groupnames from formhub/ona jsonarray output :: ie. transform ../../../var_name --> var_name on each item
+
+import json
+import sys
+import re
+
+data = json.loads(open(sys.argv[1]).read())
+
+cleanedData = []
+
+def jsonArrayKeyShortener(jsonArray):
+    outputJsonArray = []
+    for item in jsonArray:
+        #print(json.dumps(jsonArray))
+        cleanedItem = {}
+        for item_1, val_1 in item.items():
+            if type(val_1) is (str or int or float or long or complex): 
+                cleanedItem[re.sub(r'(^.).*(.)/(\w+$)', r'\3', item_1)] = val_1
+            elif type(val_1) is list:
+                if len(val_1) and type(val_1[0]) is dict:
+                    cleanedItem[re.sub(r'(^.).*(.)/(\w+$)', r'\3', item_1)] = jsonArrayKeyShortener(val_1)
+                else:
+                    cleanedItem[re.sub(r'(^.).*(.)/(\w+$)', r'\3', item_1)] = val_1
+        outputJsonArray.append(cleanedItem)
+    return outputJsonArray
+
+
+print(json.dumps(jsonArrayKeyShortener(data)))
+
+
+
+
+#for item in data:
+#    #print(item)
+#    cleanedItem = {}
+#    for item_1, val_1 in item.items():
+#        if type(val_1) is (str or int or float or long or complex):
+#            cleanedItem[re.sub(r'(^.).*(.)/(\w+$)', r'\3', item_1)] = val_1
+#        else if type(val_1) is list:
+#            
+
+
+
+
